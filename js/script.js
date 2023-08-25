eba2d9da78911173b4c2650890a1f0e9e68a723a
jasyyyan <jasmine.yip1031@gmail.com>
2023-08-25 20:21:21 +1000

    Add vertical tabs

--- js/script.js
+++ js/script.js
@@ -55,3 +55,22 @@ document.addEventListener('DOMContentLoaded', function() {
     // observe all sections
     sections.forEach(section => observer.observe(section));
 });
+
+
+/* leadership vertical tabs */
+$(document).on("click", ".naccs .menu div", function() {
+	var numberIndex = $(this).index();
+
+	if (!$(this).hasClass("active")) {
+		$(".naccs .menu div").removeClass("active");
+		$(".naccs ul li").removeClass("active");
+
+		$(this).addClass("active");
+		$(".naccs ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+		var listItemHeight = $(".naccs ul")
+			.find("li:eq(" + numberIndex + ")")
+			.innerHeight();
+		$(".naccs ul").height(listItemHeight + "px");
+	}
+});
