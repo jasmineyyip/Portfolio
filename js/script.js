68830cede8f690c658f5f490435c4d841a93a2e8
jasyyyan <jasmine.yip1031@gmail.com>
2023-08-25 17:30:53 +1000

    Add about me

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ menu.onclick = () => {
     menu.classList.toggle('fa-times');
     navbar.classList.toggle('active');
 }
+
+/* typewriter effect with alterating text */
+var typed = new Typed(".auto-type", {
+    strings: ["Web Developer.", "Computer Science student.", "Trojan."],
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+})
