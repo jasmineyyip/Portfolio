288f27c83bbf3c140ff959b7a245c6e0a698ac94
jasyyyan <yippie.web@gmail.com>
2024-06-04 23:59:54 -0700

    content updates

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ const CACHE_NAME = 'site-static-v1'; // Increment this version as needed
 const ASSETS = [
     '/index.html',
     '/css/style.css',
-    '/js/script.js'
+    '/js/main.js'
 ];
 
 // Install event - caches assets
